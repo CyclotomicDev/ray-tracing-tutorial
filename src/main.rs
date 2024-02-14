1ad3ff990a1ffc3bb623d715b676db83239c7747
Cyclotomic <CyclotomicDev@protonmail.com>
2024-02-14 03:22:42 +0000

    Camera focus and orientation

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,30 @@ fn main() {
     //World
     let mut world = HittableCollection::new();
 
-    let material_left: Rc::<dyn Material> = Rc::new(Lambertian::new(&vector![0.0,0.0,1.0]));
-    let material_right: Rc::<dyn Material> = Rc::new(Lambertian::new(&vector![1.0,0.0,0.0]));
-    //let material_left: Rc::<dyn Material> = Rc::new(Dielectric::new(1.5));
-    //let material_right: Rc::<dyn Material> = Rc::new(Metal::new(&vector![0.8,0.6,0.2], 0.0));
+    let material_ground: Rc::<dyn Material> = Rc::new(Lambertian::new(&vector![0.8,0.8,0.0]));
+    let material_center: Rc::<dyn Material> = Rc::new(Lambertian::new(&vector![0.1,0.2,0.5]));
+    let material_left: Rc::<dyn Material> = Rc::new(Dielectric::new(1.5));
+    let material_right: Rc::<dyn Material> = Rc::new(Metal::new(&vector![0.8,0.6,0.2], 0.0));
 
-    let r = (PI / 4.0).cos();
+    //let r = (PI / 4.0).cos();
 
-    world.push(Rc::new(Sphere::new(vector![-r,0.0,-1.0], r,  &(material_left))));
-    world.push(Rc::new(Sphere::new(vector![r,0.0,-1.0], r, &material_right)));
-    //world.push(Rc::new(Sphere::new(vector![-1.0,0.0,-1.0], 0.5, &material_left)));
-    //world.push(Rc::new(Sphere::new(vector![-1.0,0.0,-1.0], -0.4, &material_left)));
-    //world.push(Rc::new(Sphere::new(vector![1.0, 0.0,-1.0], 0.5, &material_right)));
+    world.push(Rc::new(Sphere::new(vector![0.0,-100.5,-1.0], 100.0,  &(material_ground))));
+    world.push(Rc::new(Sphere::new(vector![0.0,0.0,-1.0], 0.5, &material_center)));
+    world.push(Rc::new(Sphere::new(vector![-1.0,0.0,-1.0], 0.5, &material_left)));
+    world.push(Rc::new(Sphere::new(vector![-1.0,0.0,-1.0], -0.4, &material_left)));
+    world.push(Rc::new(Sphere::new(vector![1.0, 0.0,-1.0], 0.5, &material_right)));
     
     
-    let mut camera = Camera::new(16.0 / 9.0, 400);
+    let mut camera = Camera::new(
+        16.0 / 9.0, 
+        400,
+        100,
+        50,
+        20.0,
+        vector![-2.0,2.0,1.0], 
+        vector![0.0,0.0,-1.0], 
+        vector![0.0,1.0,0.0]
+    );
     
     camera.render(&world);
 }
@@ -208,6 +217,8 @@ impl Interval {
 const empty: Interval =  Interval{min: f32::INFINITY, max:-f32::INFINITY};
 const universal: Interval =  Interval{min:-f32::INFINITY, max:f32::INFINITY};
 
+struct Basis(Direction,Direction,Direction);
+
 struct Camera {
     //Image 
     aspect_ratio: f32,
@@ -221,27 +232,39 @@ struct Camera {
     rng: ThreadRng,
     max_depth: usize,
     v_fov: f32, //Vertical field of view in degrees
+    look_from: Direction,
+    look_at: Direction,
+    v_up: Direction,
+    basis: Basis,
 }
 
 impl Camera {
-    fn new(aspect_ratio: f32, image_width: u32) -> Self {
+    fn new(aspect_ratio: f32, image_width: u32, samples_per_pixel: usize, max_depth: usize, v_fov: f32, look_from: Direction, look_at: Direction, v_up: Direction) -> Self {
         //Calculate image height: 1) round down 2) ensure at least 1
         let image_height = (image_width as f32/ aspect_ratio) as i32;
         let image_height: u32 = if image_height < 1 {1} else {image_height as u32};
 
-        let center = vector![0.0,0.0,0.0];
+        let center = look_from.clone();
+
+        let dif = look_from.clone() - &look_at;
 
         //Determine viewport dimensions
-        let focal_length = 1.0;
-        let v_fov = 90.0;
+        let focal_length = dif.norm();
         let theta = v_fov * PI / 180.0;
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h * focal_length;
         let viewport_width = viewport_height * ((image_width as f32) / (image_height as f32));
 
+        let w = vec_unit(&dif);
+        let u = vec_unit(&v_up.clone().cross(&w));
+        let v = w.clone().cross(&u);
+
+        let basis = Basis(w,u,v);
+
+
         //Calculate vectors across the horizontal and down vertical viewport edges
-        let viewport_u: Point = vector![viewport_width, 0.0, 0.0];
-        let viewport_v: Point = vector![0.0, -viewport_height, 0.0];
+        let viewport_u: Point = viewport_width * basis.1;
+        let viewport_v: Point = viewport_height * -basis.2;
 
         //Calculate horizontal and vertical delta (pixel size)
         let pixel_delta_u: Direction = viewport_u / (image_width as f32);
@@ -250,21 +273,17 @@ impl Camera {
         //Calculate location of upper left pixel
         let viewport_start = 
             center.clone()
-            - vector![0.0, 0.0, focal_length]
+            - (focal_length * basis.0)
             - (viewport_u / 2.0)
             - (viewport_v / 2.0);
     
         let pixel_start =
             viewport_start + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-        let samples_per_pixel = 100;
-
         let rng = rand::thread_rng();
 
-        let max_depth = 50;
 
-
-        Self {aspect_ratio, image_width, image_height, center, pixel_start, pixel_delta_u, pixel_delta_v, samples_per_pixel, rng, max_depth, v_fov}
+        Self {aspect_ratio, image_width, image_height, center, pixel_start, pixel_delta_u, pixel_delta_v, samples_per_pixel, rng, max_depth, v_fov, look_from,look_at,v_up, basis}
     }
 
     fn render(&mut self, world: &dyn Hittable) {
